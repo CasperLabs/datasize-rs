702425f566090e568449319789e6ae9ce9147d9b
Marc Brinkmann <marc@casperlabs.io>
2020-09-26 13:52:53 +0200

    Turn example into test

--- datasize/src/lib.rs
+++ datasize/src/lib.rs
@@ -300,6 +300,7 @@ where
 
 #[cfg(test)]
 mod tests {
+    use crate as datasize; // Required for the derive macro.
     use crate::{data_size, DataSize};
 
     #[test]
@@ -324,5 +325,42 @@ mod tests {
 
         assert_eq!(data_size::<Option<Box<u64>>>(&value_none), 0);
         assert_eq!(data_size::<Option<Box<u64>>>(&value_some), 8);
+    }
+
+    #[test]
+    fn test_struct() {
+        #[derive(DataSize)]
+        struct Example {
+            count: usize,
+            my_data: Vec<MyStruct>,
+            warning: Option<Box<u32>>,
+            #[data_size(skip)]
+            #[allow(dead_code)]
+            skipped: Box<char>,
+        }
+
+        #[derive(DataSize)]
+        struct MyStruct {
+            count: u64,
+        }
+
+        // Start with a small example struct.
+        let mut ex = Example {
+            count: 99,
+            my_data: vec![],
+            warning: None,
+            skipped: Default::default(),
+        };
+
+        // We expect a heap size of 0, as the vec is empty and no box allocated.
+        assert_eq!(data_size(&ex), 0);
+
+        // Add a `warning` will cause a heap allocation.
+        ex.warning = Some(Box::new(12345));
+        assert_eq!(data_size(&ex), 4);
+
+        // Let's reserve some capacity on `my_data`.
+        ex.my_data.reserve_exact(10);
+        assert_eq!(data_size(&ex), 4 + 10 * 8)
     }
 }
