16a7fbdb00c11d846861bd18d3971194589f9786
Marc Brinkmann <marc@casperlabs.io>
2020-09-26 20:09:37 +0200

    Support tokio `JoinHandle`

--- datasize/src/tokio.rs
+++ datasize/src/tokio.rs
@@ -19,3 +19,13 @@ impl<T> DataSize for tokio::sync::oneshot::Receiver<T> {
         0
     }
 }
+
+impl<T> DataSize for tokio::task::JoinHandle<T> {
+    const IS_DYNAMIC: bool = false;
+    const STATIC_HEAP_SIZE: usize = 0;
+
+    #[inline]
+    fn estimate_heap_size(&self) -> usize {
+        0
+    }
+}
